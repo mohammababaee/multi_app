9b30094387b757cdf9b00b75385ef5f105a280e8
mohammadbabaeesem <mohammadbabaeesem@gmail.com>
2024-05-12 08:36:52 +0330

    chore: change structure of code and add readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# multi_app
+# Web Scraping News from Varzesh3
+
+This Python script scrapes the latest news headlines and links from the Varzesh3 website and prints them to the console.
+
+## Requirements
+
+- Python 
+- BeautifulSoup4
+- Requests
+
+## Installation
+
+1. Clone the repository:
+
+2. Install dependencies:
+    ```
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+1. Run the script:
+    ```
+    python scrape_news.py
+    ```
+
+2. The script will fetch the latest news headlines and links from the Varzesh3 website and print them to the console.
+
+## Customization
+
+You can customize the number of news articles fetched by modifying the `[:20]` slice in the code to your desired number.
--- news_collector.py
+++ news_collector.py
@@ -1,25 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def get_news(url):
+    # Fetch the webpage content
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Find all elements with class="newsbox-2"
+    newsbox_2_elements = soup.find_all("div", class_="newsbox-2")[:20]
+
+    for news_box in newsbox_2_elements:
+        # Extract relevant information
+        news_content = news_box.find("div", class_="news-content")
+        title = news_content.find("a", class_="title").find("h3").text
+        news_data = news_content.find("div", class_="news-data")
+        time = news_data.find("time").text.strip()
+        link = news_content.find("a", class_="title")["href"]
+
+        # Print the extracted data
+        print("Time:", time)
+        print("Title:", title)
+        print("Link:", link)
+        print("-" * 30)
+
+
 # URL of the webpage
-url = "https://www.varzesh3.com/news"
-
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-
-# Find all elements with class="newsbox-2"
-newsbox_2_elements = soup.find_all("div", class_="newsbox-2")[:20]
-
-for news_box in newsbox_2_elements:
-
-    news_content = news_box.find("div", class_="news-content")
-    title = news_content.find("a", class_="title").find("h3").text
-    news_data = news_content.find("div", class_="news-data")
-    time = news_data.find("time").text.strip()
-
-    link = news_content.find("a", class_="title")["href"]
-
-    print("Time:", time)
-    print("Title:", title)
-    print("Link:", link)
-    print("-" * 30)
+url = "https://www.varzesh3.com/news"  # It's not recommended to hardcode URLs directly into your application like this. While it's acceptable at the development stage, in production, it's better to use environment variables or configuration files to manage such URLs securely.
+get_news(url)
